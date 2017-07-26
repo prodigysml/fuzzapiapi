f79b55c3fbf11e1c0268bf7a32165182fd8544a7
Sajeeb Lohani <saloh2@student.monash.edu>
2017-07-26 21:14:55 +1000

    Added in the searching API function

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Fuzzapi's easy to use Python API
 
 # Features
-* Searching for scans
+* Searching for scans - Use of SQL Wildcards (%) may be necessary
 * Start scan
 * Get results
--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from db import run_db_query
 
 app = Flask(__name__)
@@ -8,6 +9,33 @@ app.config["DBPATH"] = "/root/Documents/HackingTools/fuzzapi/db/development.sqli
 @app.route("/scans/all", methods=["GET"])
 def get_all_scans():
     return run_db_query(app.config["DBPATH"], "SELECT * FROM SCANS")
+
+
+@app.route("/scans/search", methods=["GET"])
+def search_all_scans():
+    # flag to add where clause to query
+    where_clause_present = False
+
+    # base query
+    query = "SELECT * FROM SCANS"
+
+    args = ["id", "url", "sid", "parameters", "method", "cookies", "created_at", "updated_at", "json", "user_id", "status"]
+
+    params = dict()
+
+    for arg in args:
+        get_variable = request.args.get(arg)
+
+        if get_variable is not None:
+            if not where_clause_present:
+                query += " WHERE"
+                where_clause_present = True
+
+            query += " " + arg + " LIKE :" + arg
+
+            params[arg] = get_variable
+
+    return run_db_query(app.config["DBPATH"], query, params)
 
 
 if __name__ == "__main__":
