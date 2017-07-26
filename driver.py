76a8d9ef4ce7e68f4dce91792ad3f826e23d563c
Sajeeb Lohani <saloh2@student.monash.edu>
2017-07-26 19:35:45 +1000

    Added in a retrieve all scans for the API

--- driver.py
+++ driver.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask import Response
+import sqlite3
+import json
 
 app = Flask(__name__)
+app.config["DBPATH"] = "/root/Documents/HackingTools/fuzzapi/db/development.sqlite3"
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/scans/all", methods=["GET"])
+def get_all_scans():
+    conn = sqlite3.connect(app.config["DBPATH"])
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM SCANS")
+
+    response = Response(json.dumps(cursor.fetchall()))
+
+    response.headers['Content-type'] = "application/json"
+
+    return response
 
 
 if __name__ == "__main__":
